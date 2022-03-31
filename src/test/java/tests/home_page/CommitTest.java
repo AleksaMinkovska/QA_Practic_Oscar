56402882b95145572fd8ed756deaccfab9d6b42d
AleksaMinkovska <aleksa.minkovska@gmail.com>
2022-03-31 15:28:02 +0300

    Create CommitTest.java

---
+++ src/test/java/tests/home_page/CommitTest.java
@@ -0,0 +1,14 @@
+package tests.home_page;
+
+import org.testng.annotations.Test;
+import tests.TestBase;
+
+public class CommitTest extends TestBase{
+
+    @Test
+    public void commitTest(){
+
+        System.out.println("Test for branch commit");
+    }
+
+}
